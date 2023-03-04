16cdecf897012d7c9c99b49af7247011eb1530bb
Jeffery Utter <jeff@jeffutter.com>
2023-03-03 22:26:35 -0600

    mdns for discovery

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,4 +1,4 @@
 #[tokio::main]
 async fn main() {
-    net::main().await;
+    net::main().await.unwrap();
 }
--- net/src/lib.rs
+++ net/src/lib.rs
@@ -1,7 +1,8 @@
 use libp2p::futures::StreamExt;
 use libp2p::swarm::{keep_alive, NetworkBehaviour, Swarm, SwarmEvent};
-use libp2p::{identity, ping, Multiaddr, PeerId};
+use libp2p::{identity, mdns, ping, PeerId};
 use std::error::Error;
+use void;
 
 pub async fn main() -> Result<(), Box<dyn Error>> {
     let local_key = identity::Keypair::generate_ed25519();
@@ -10,23 +11,31 @@ pub async fn main() -> Result<(), Box<dyn Error>> {
 
     let transport = libp2p::tokio_development_transport(local_key)?;
 
-    let mut swarm = Swarm::with_tokio_executor(transport, Behaviour::default(), local_peer_id);
+    let mdns = mdns::tokio::Behaviour::new(mdns::Config::default(), local_peer_id)?;
+
+    let mut swarm = Swarm::with_tokio_executor(
+        transport,
+        Behaviour {
+            ping: ping::Behaviour::default(),
+            keep_alive: keep_alive::Behaviour::default(),
+            mdns,
+        },
+        local_peer_id,
+    );
 
     // Tell the swarm to listen on all interfaces and a random, OS-assigned
     // port.
     swarm.listen_on("/ip4/0.0.0.0/tcp/0".parse()?)?;
 
-    // Dial the peer identified by the multi-address given as the second
-    // command-line argument, if any.
-    if let Some(addr) = std::env::args().nth(1) {
-        let remote: Multiaddr = addr.parse()?;
-        swarm.dial(remote)?;
-        println!("Dialed {addr}")
-    }
-
     loop {
         match swarm.select_next_some().await {
             SwarmEvent::NewListenAddr { address, .. } => println!("Listening on {address:?}"),
+            SwarmEvent::Behaviour(MyBehaviourEvent::Mdns(mdns::Event::Discovered(addrs))) => {
+                for (id, addr) in addrs {
+                    swarm.dial(addr)?;
+                    println!("Dialed {id}");
+                }
+            }
             SwarmEvent::Behaviour(event) => println!("{event:?}"),
             _ => {}
         }
@@ -37,8 +46,36 @@ pub async fn main() -> Result<(), Box<dyn Error>> {
 ///
 /// For illustrative purposes, this includes the [`KeepAlive`](behaviour::KeepAlive) behaviour so a continuous sequence of
 /// pings can be observed.
-#[derive(NetworkBehaviour, Default)]
+#[derive(NetworkBehaviour)]
+#[behaviour(out_event = "MyBehaviourEvent")]
 struct Behaviour {
     keep_alive: keep_alive::Behaviour,
     ping: ping::Behaviour,
+    mdns: mdns::tokio::Behaviour,
+}
+
+#[derive(Debug)]
+#[allow(clippy::large_enum_variant)]
+enum MyBehaviourEvent {
+    Mdns(mdns::Event),
+    Ping(ping::Event),
+    Void,
+}
+
+impl From<mdns::Event> for MyBehaviourEvent {
+    fn from(event: mdns::Event) -> Self {
+        MyBehaviourEvent::Mdns(event)
+    }
+}
+
+impl From<ping::Event> for MyBehaviourEvent {
+    fn from(event: ping::Event) -> Self {
+        MyBehaviourEvent::Ping(event)
+    }
+}
+
+impl From<void::Void> for MyBehaviourEvent {
+    fn from(_: void::Void) -> Self {
+        MyBehaviourEvent::Void
+    }
 }
