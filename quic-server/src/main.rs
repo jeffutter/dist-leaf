5e0c4a4c30af9817590ed186fe1b619b8901a8d7
Jeffery Utter <jeff@jeffutter.com>
2023-03-18 22:31:31 -0500

    Cleanup of quic-server

--- quic-server/src/main.rs
+++ quic-server/src/main.rs
@@ -1,20 +1,24 @@
 use bytes::Bytes;
 use consistent_hash_ring::{Ring, RingBuilder};
-use mdns_sd::{ServiceDaemon, ServiceEvent, ServiceInfo};
-use s2n_quic::{client::Connect, stream::BidirectionalStream, Client, Connection, Server};
+use mdns_sd::{Receiver, ServiceDaemon, ServiceEvent, ServiceInfo};
+use s2n_quic::{
+    client::Connect, connection, stream::BidirectionalStream, Client, Connection, Server,
+};
 use std::{
     collections::HashMap,
     error::Error,
+    future::Future,
     net::{Ipv4Addr, SocketAddr},
-    process,
     sync::Arc,
     time::Duration,
 };
+use thiserror::Error;
 use tokio::{
     sync::RwLock,
     task,
     time::{self, Instant},
 };
+use uuid::Uuid;
 
 /// NOTE: this certificate is to be used for demonstration purposes only!
 pub static CERT_PEM: &str = include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "/../cli/cert.pem"));
@@ -30,7 +34,7 @@ struct S2SConnection {
 }
 
 impl S2SConnection {
-    async fn new(addr: SocketAddr, client: Client) -> Result<Self, Box<dyn Error>> {
+    async fn new(addr: SocketAddr, client: Client) -> Result<Self, ServerError> {
         let connect = Connect::new(addr).with_server_name("localhost");
         let mut connection = client.connect(connect).await?;
         connection.keep_alive(true)?;
@@ -75,7 +79,7 @@ impl S2SConnections {
         }
     }
 
-    async fn add(&mut self, addr: SocketAddr) -> Result<(), Box<dyn Error>> {
+    async fn add(&mut self, addr: SocketAddr) -> Result<(), ServerError> {
         if let None = self.connections.get(&addr) {
             let s2s_connection = S2SConnection::new(addr, self.client.clone()).await?;
             self.connections.insert(addr, s2s_connection);
@@ -96,6 +100,228 @@ impl S2SConnections {
     }
 }
 
+#[derive(Error, Debug)]
+pub enum ServerError {
+    #[error("connection error")]
+    Connection(#[from] s2n_quic::connection::Error),
+    #[error("unknown server error")]
+    Unknown,
+}
+
+struct VNode {
+    connections: Arc<RwLock<S2SConnections>>,
+    storage: db::Database,
+    socket_addr: SocketAddr,
+    mdns_receiver: Receiver<ServiceEvent>,
+    server: s2n_quic::Server,
+}
+
+impl VNode {
+    fn new(id: Uuid, local_ip: Ipv4Addr) -> Result<Self, Box<dyn Error>> {
+        let mdns = ServiceDaemon::new().expect("Failed to create daemon");
+
+        let service_type = "_mdns-quic-db._udp.local.";
+        let receiver = mdns.browse(service_type).expect("Failed to browse");
+
+        let server = Server::builder()
+            .with_tls((CERT_PEM, KEY_PEM))?
+            .with_io("0.0.0.0:0")?
+            .start()?;
+
+        let instance_name = format!("node-{}", id);
+        let host_ipv4: Ipv4Addr = local_ip;
+        let host_name = format!("{}.local.", host_ipv4);
+        let port = server.local_addr()?.port();
+        let my_service = ServiceInfo::new(
+            service_type,
+            &instance_name,
+            &host_name,
+            host_ipv4,
+            port,
+            None,
+        )
+        .unwrap();
+
+        mdns.register(my_service.clone())
+            .expect("Failed to register our service");
+
+        let socket_addr: SocketAddr = format!(
+            "{}:{}",
+            my_service.get_addresses().iter().next().unwrap(),
+            my_service.get_port()
+        )
+        .parse()
+        .unwrap();
+
+        let client = Client::builder()
+            .with_tls(CERT_PEM)?
+            .with_io("0.0.0.0:0")?
+            .start()?;
+
+        let connections = S2SConnections::new(client, socket_addr);
+        let connections = Arc::new(RwLock::new(connections));
+        let storage = db::Database::new_tmp();
+
+        Ok(Self {
+            connections,
+            storage,
+            socket_addr,
+            server,
+            mdns_receiver: receiver,
+        })
+    }
+
+    fn mdns_loop(
+        connections: Arc<RwLock<S2SConnections>>,
+        local_socket_addr: SocketAddr,
+        mdns_receiver: Receiver<ServiceEvent>,
+    ) -> impl Future<Output = Result<(), ServerError>> {
+        async move {
+            let mut interval = time::interval(Duration::from_secs(30));
+
+            loop {
+                interval.tick().await;
+                while let Ok(event) = mdns_receiver.recv_async().await {
+                    match event {
+                        ServiceEvent::ServiceResolved(info) => {
+                            println!(
+                                "Resolved a new service: {} {}",
+                                info.get_fullname(),
+                                info.get_port()
+                            );
+
+                            let socket_addr: SocketAddr = SocketAddr::new(
+                                info.get_addresses()
+                                    .iter()
+                                    .next()
+                                    .unwrap()
+                                    .to_owned()
+                                    .into(),
+                                info.get_port(),
+                            );
+
+                            if socket_addr != local_socket_addr {
+                                connections.write().await.add(socket_addr).await?;
+                            }
+                        }
+                        other_event => {
+                            println!("Received other event: {:?}", &other_event);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    fn handle_stream(
+        mut stream: BidirectionalStream,
+        connections: Arc<RwLock<S2SConnections>>,
+        storage: db::Database,
+    ) -> impl Future<Output = ()> {
+        async move {
+            while let Ok(Some(data)) = stream.receive().await {
+                println!("New data");
+                let start = Instant::now();
+                let req = net::decode_request(data.as_ref()).unwrap();
+                println!(
+                    "decoded: {}µs",
+                    Instant::now().duration_since(start).as_micros()
+                );
+
+                match req {
+                    net::KVRequestType::Get(key) => match connections.write().await.get(&key) {
+                        Destination::Remote(connection) => {
+                            println!("Forward to: {:?}", connection);
+                            connection.send(data).await.unwrap();
+                            let data = connection.recv().await.unwrap().unwrap();
+                            stream.send(data).await.expect("stream should be open");
+                        }
+                        Destination::Local => {
+                            println!("Serve Local");
+                            println!(
+                                "checked connection: {}µs",
+                                Instant::now().duration_since(start).as_micros()
+                            );
+                            let res_data = storage.get(&key).unwrap();
+                            println!(
+                                "fetched: {}µs",
+                                Instant::now().duration_since(start).as_micros()
+                            );
+                            let res = net::encode_response(net::KVResponseType::Result(res_data));
+                            println!(
+                                "encoded: {}µs",
+                                Instant::now().duration_since(start).as_micros()
+                            );
+                            stream.send(res).await.expect("stream should be open");
+                            println!(
+                                "sent: {}µs",
+                                Instant::now().duration_since(start).as_micros()
+                            );
+                        }
+                    },
+                    net::KVRequestType::Put(key, value) => {
+                        match connections.write().await.get(&key) {
+                            Destination::Remote(connection) => {
+                                connection.send(data).await.unwrap();
+                                let data = connection.recv().await.unwrap().unwrap();
+                                stream.send(data).await.expect("stream should be open");
+                            }
+                            Destination::Local => {
+                                storage.put(&key, &value).unwrap();
+                                let res = net::encode_response(net::KVResponseType::Ok);
+                                stream.send(res).await.expect("stream should be open");
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    fn handle_connection(
+        mut connection: connection::Connection,
+        connections: Arc<RwLock<S2SConnections>>,
+        storage: db::Database,
+    ) -> impl Future<Output = ()> {
+        async move {
+            println!("New Connection");
+            while let Ok(Some(stream)) = connection.accept_bidirectional_stream().await {
+                println!("New Stream");
+                tokio::spawn(Self::handle_stream(
+                    stream,
+                    connections.clone(),
+                    storage.clone(),
+                ));
+            }
+        }
+    }
+
+    async fn run(&mut self) -> Result<(), Box<dyn Error>> {
+        let storage = self.storage.clone();
+        let mdns_receiver = self.mdns_receiver.clone();
+        let local_socket_addr = self.socket_addr;
+
+        let mdns_search = task::spawn(Self::mdns_loop(
+            self.connections.clone(),
+            local_socket_addr,
+            mdns_receiver,
+        ));
+
+        while let Some(connection) = self.server.accept().await {
+            // spawn a new task for the connection
+            tokio::spawn(Self::handle_connection(
+                connection,
+                self.connections.clone(),
+                storage.clone(),
+            ));
+        }
+
+        mdns_search.await??;
+
+        Ok(())
+    }
+}
+
 enum Destination<'a> {
     Remote(&'a mut S2SConnection),
     Local,
@@ -103,172 +329,16 @@ enum Destination<'a> {
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    let mdns = ServiceDaemon::new().expect("Failed to create daemon");
-
-    let service_type = "_mdns-quic-db._udp.local.";
-    let receiver = mdns.browse(service_type).expect("Failed to browse");
-
-    let mut server = Server::builder()
-        .with_tls((CERT_PEM, KEY_PEM))?
-        .with_io("0.0.0.0:0")?
-        .start()?;
-
     let local_ip = match local_ip_address::local_ip()? {
         std::net::IpAddr::V4(ip) => ip,
         std::net::IpAddr::V6(_) => todo!(),
     };
 
-    let instance_name = format!("my_instance-{}", process::id());
-    let host_ipv4: Ipv4Addr = local_ip;
-    let host_name = format!("{}.local.", host_ipv4);
-    let port = server.local_addr()?.port();
-    let my_service = ServiceInfo::new(
-        service_type,
-        &instance_name,
-        &host_name,
-        host_ipv4,
-        port,
-        None,
-    )
-    .unwrap();
+    let id = Uuid::new_v4();
 
-    mdns.register(my_service.clone())
-        .expect("Failed to register our service");
+    let mut vnode = VNode::new(id, local_ip)?;
 
-    let server_socket_addr: SocketAddr = format!(
-        "{}:{}",
-        my_service.get_addresses().iter().next().unwrap(),
-        my_service.get_port()
-    )
-    .parse()
-    .unwrap();
-
-    let client = Client::builder()
-        .with_tls(CERT_PEM)?
-        .with_io("0.0.0.0:0")?
-        .start()?;
-
-    let connections = S2SConnections::new(client, server_socket_addr);
-    let connections = Arc::new(RwLock::new(connections));
-
-    let connections1 = connections.clone();
-    let mdns_search = task::spawn(async move {
-        let mut interval = time::interval(Duration::from_secs(30));
-
-        loop {
-            interval.tick().await;
-            while let Ok(event) = receiver.recv_async().await {
-                match event {
-                    ServiceEvent::ServiceResolved(info) => {
-                        println!(
-                            "Resolved a new service: {} {}",
-                            info.get_fullname(),
-                            info.get_port()
-                        );
-
-                        let socket_addr: SocketAddr = SocketAddr::new(
-                            info.get_addresses()
-                                .iter()
-                                .next()
-                                .unwrap()
-                                .to_owned()
-                                .into(),
-                            info.get_port(),
-                        );
-
-                        if socket_addr != server_socket_addr {
-                            connections1.write().await.add(socket_addr).await.unwrap();
-                        }
-                    }
-                    other_event => {
-                        println!("Received other event: {:?}", &other_event);
-                    }
-                }
-            }
-        }
-    });
-
-    let storage = db::Database::new_tmp();
-
-    while let Some(mut connection) = server.accept().await {
-        let connections1 = connections.clone();
-        let storage1 = storage.clone();
-        // spawn a new task for the connection
-        tokio::spawn(async move {
-            println!("New Connection");
-            while let Ok(Some(mut stream)) = connection.accept_bidirectional_stream().await {
-                println!("New Stream");
-                let connections2 = connections1.clone();
-                let storage2 = storage1.clone();
-                // spawn a new task for the stream
-                tokio::spawn(async move {
-                    // echo any data back to the stream
-                    while let Ok(Some(data)) = stream.receive().await {
-                        println!("New data");
-                        let start = Instant::now();
-                        let req = net::decode_request(data.as_ref()).unwrap();
-                        println!(
-                            "decoded: {}µs",
-                            Instant::now().duration_since(start).as_micros()
-                        );
-
-                        match req {
-                            net::KVRequestType::Get(key) => {
-                                match connections2.write().await.get(&key) {
-                                    Destination::Remote(connection) => {
-                                        println!("Forward to: {:?}", connection);
-                                        connection.send(data).await.unwrap();
-                                        let data = connection.recv().await.unwrap().unwrap();
-                                        stream.send(data).await.expect("stream should be open");
-                                    }
-                                    Destination::Local => {
-                                        println!("Serve Local");
-                                        println!(
-                                            "checked connection: {}µs",
-                                            Instant::now().duration_since(start).as_micros()
-                                        );
-                                        let res_data = storage2.get(&key).unwrap();
-                                        println!(
-                                            "fetched: {}µs",
-                                            Instant::now().duration_since(start).as_micros()
-                                        );
-                                        let res = net::encode_response(
-                                            net::KVResponseType::Result(res_data),
-                                        );
-                                        println!(
-                                            "encoded: {}µs",
-                                            Instant::now().duration_since(start).as_micros()
-                                        );
-                                        stream.send(res).await.expect("stream should be open");
-                                        println!(
-                                            "sent: {}µs",
-                                            Instant::now().duration_since(start).as_micros()
-                                        );
-                                    }
-                                }
-                            }
-                            net::KVRequestType::Put(key, value) => {
-                                match connections2.write().await.get(&key) {
-                                    Destination::Remote(connection) => {
-                                        connection.send(data).await.unwrap();
-                                        let data = connection.recv().await.unwrap().unwrap();
-                                        stream.send(data).await.expect("stream should be open");
-                                    }
-                                    Destination::Local => {
-                                        storage2.put(&key, &value).unwrap();
-                                        let res = net::encode_response(net::KVResponseType::Ok);
-                                        stream.send(res).await.expect("stream should be open");
-                                    }
-                                }
-                            }
-                        }
-                    }
-                });
-            }
-        });
-    }
-
-    mdns_search.await?;
+    vnode.run().await?;
 
     Ok(())
 }
